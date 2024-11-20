b6cd7395dbe95d5ee2b799fc17f7a57837de8ce7
Andrey Tsimbalov <@tsimbalov.ae>
2024-11-20 21:01:43 +0300

    Настроено получение курса валют

--- twirp_client.py
+++ twirp_client.py
@@ -6,7 +6,7 @@ import haberdasher_twirp, haberdasher_pb2
 client = haberdasher_twirp.HaberdasherClient("http://localhost:3000")
 
 try:
-    response = client.GetDollarRate(ctx=Context(), request=haberdasher_pb2.ValuteId(id="12"))
+    response = client.GetDollarRate(ctx=Context(), request=haberdasher_pb2.ValuteId(id="R01235"))
     print(response)
 except TwirpServerException as e:
     print(e.code, e.message, e.meta, e.to_dict())
--- twirp_server.py
+++ twirp_server.py
@@ -1,15 +1,41 @@
-import random
-
+from xml.etree.ElementTree import XML, fromstring
+import aiohttp
 from twirp.asgi import TwirpASGIApp
 
 import haberdasher_twirp, haberdasher_pb2
 
+
+def find_valute_by_id(xml_object: XML, id: str) -> (str, str, float):
+    name = None
+    char_code = None
+    value = None
+    for elem in xml_object:
+        if elem.attrib["ID"] == id:
+            for field in elem:
+                if field.tag == 'Name':
+                    name = field.text
+                if field.tag == 'CharCode':
+                    char_code = field.text
+                if field.tag == 'Value':
+                    value = float(field.text.replace(',', '.'))
+            break
+    if None in (name, char_code, value):
+        raise Exception(f"Not coeerct ValuteId.id: {id}")
+    return name, char_code, value
+
+
 class HaberdasherService(object):
-    def GetDollarRate(self, context, valute_id):
+    async def GetDollarRate(self, context, valute_id):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://www.cbr.ru/scripts/XML_daily.asp') as response:
+                text = await response.text()
+                result_xml = fromstring(text)
+                name, char_code, value = find_valute_by_id(result_xml, valute_id.id)
+
         return haberdasher_pb2.DollarRate(
-            name=random.choice(["bowler", "baseball cap", "top hat", "derby"]),
-            char_code=random.choice(["white", "black", "brown", "red", "blue"]),
-            value=123.1,
+            name=name,
+            char_code=char_code,
+            value=value,
         )
 
 
